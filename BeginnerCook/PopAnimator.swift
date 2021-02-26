6a70e8b80b2f1c34f52bdf611e17c0ba182a1164
Lucas <lucaspedrazoli@gmail.com>
2021-02-26 12:12:39 -0300

    create pop animator

--- BeginnerCook/PopAnimator.swift
+++ BeginnerCook/PopAnimator.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+import UIKit
+
+class PopAnimator: NSObject,
+UIViewControllerAnimatedTransitioning {
+
+  func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+    return 0
+  }
+
+  func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+
+  }
+}
