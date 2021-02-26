e4b872b63effb919182a00a06869a039e4fe2ada
Lucas <lucaspedrazoli@gmail.com>
2021-02-26 13:27:58 -0300

    add pop animation to open scroll items

--- BeginnerCook/PopAnimator.swift
+++ BeginnerCook/PopAnimator.swift
@@ -21,7 +21,47 @@ UIViewControllerAnimatedTransitioning {
   }
 
   func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-    
+    let containerView = transitionContext.containerView
+    let herbView = presenting ?
+      transitionContext.view(forKey: .to)! :
+      transitionContext.view(forKey: .from)!
+
+    let initialFrame = presenting ? originFrame : herbView.frame
+    let finalFrame = presenting ? herbView.frame : originFrame
+    let xScaleFactor = presenting ?
+      initialFrame.width / finalFrame.width :
+      finalFrame.width / initialFrame.width
+    let yScaleFactor = presenting ?
+      initialFrame.height / finalFrame.height :
+      finalFrame.height / initialFrame.height
+
+    let scaleTransform = CGAffineTransform(scaleX: xScaleFactor,
+                                           y: yScaleFactor)
+    if presenting {
+      herbView.transform = scaleTransform
+      herbView.center = CGPoint(x: initialFrame.midX,
+                                y: initialFrame.midY)
+      herbView.clipsToBounds = true
+    }
+
+    if let toView = transitionContext.view(forKey: .to) {
+      containerView.addSubview(toView)
+    }
+    containerView.bringSubviewToFront(herbView)
+    UIView.animate(withDuration: duration,
+                   delay: 0.0,
+                   usingSpringWithDamping: 0.4,
+                   initialSpringVelocity: 0.0,
+                   animations: {
+                    herbView.transform = self.presenting ?
+                                        CGAffineTransform.identity :
+                                        scaleTransform
+                    herbView.center = CGPoint(x: finalFrame.midX,
+                                              y:finalFrame.midY)
+                    }, completion: { _ in
+                        transitionContext.completeTransition(true)
+                      })
+
   }
 
 
